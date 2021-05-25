f4cb6a4f4f90d2891fc774be9c37daea0ae37c27
Soumay kanti Das <anikds2859@outlook.com>
2021-05-25 13:16:47 +0600

    Add project files.

---
+++ Explicit second/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+struct Digit
+{
+    byte value;
+    public Digit(byte value)
+    {
+        if (value >9)
+        {
+            throw new ArgumentException();
+        }
+        this.value = value;
+
+    }
+    // Define explicit byte-to-Digit conversion operator;
+    public static explicit operator Digit(byte b)
+    {
+        Digit d = new Digit(b);
+        Console.WriteLine("conversion occured");
+        return d;
+    }
+        
+}
+
+namespace Explicit_second
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                byte b=3;
+                Digit d = (Digit)b; //explicit conversion
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("{0} Exception caught.", e);
+            }
+        }
+    }
+}
